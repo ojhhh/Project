a7bef0400b9e9199efc8849fd912f1a5f7f8dcd7
ojhhh <winnel@naver.com>
2023-05-28 22:57:26 +0900

    commit

--- Stack_nodejs/boardclone_ohstory/routes/route_verify.js
+++ Stack_nodejs/boardclone_ohstory/routes/route_verify.js
@@ -9,8 +9,8 @@ router.post("/verify", (req, res) => {
     const token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiSldUIiwibmFtZSI6InRlc3RlciIsImlhdCI6MTY4Mzk4ODk4NiwiZXhwIjoxNjgzOTg5NTg2LCJpc3MiOiJhZG1pbiJ9.X4ni3iSw407gWJBM0EL23-3YMx2Tu4BmZ3r-4i5QU0Q";
     const key = process.env.KEY2;
-    console.log("token");
-    console.log(token);
+    // console.log("token");
+    // console.log(token);
     const ver = jwt.verify(token, key, (err, decoded) => {
       if (err) {
         console.log("route_verify error");
